ed148635264c05f9469f85485cde562a63597014
michael.jolin <michael.jolin@cauca.ca>
2017-07-17 17:15:55 -0400

    fix(password) Fix encoding when password is integer

--- core/encryption.py
+++ core/encryption.py
@@ -6,6 +6,9 @@ from ..config import setup as config
 class Encryption:
 	@staticmethod
 	def password(password):
+		if isinstance(password, int):
+			password = str(password)
+
 		secretkey = bytes(config.PACKAGE_NAME, encoding='utf-8')
 		hash = hmac.new(secretkey, password.encode('UTF-8'), hashlib.sha256)
 
