2b2bd0dc04e9b16d0268208a622cf503081987f6
michael.jolin <michael.jolin@cauca.ca>
2018-08-23 14:21:24 -0400

    refactor: Remove used file list for cache

--- core/page_with_devextreme.py
+++ core/page_with_devextreme.py
@@ -160,25 +160,6 @@ class PageWithDevextreme:
 
 	def create_cache(self, files):
 		version = config.PACKAGE_VERSION if config.PACKAGE_VERSION != '__package_version__' else datetime.now().strftime("%Y-%m-%d_%H:%M")
-		cache_files = files + [
-			'/static/cause-web-template/css/generic.cause.css',
-			'/static/cause-web-template/css/generic.light.custom-%s.css' % config.VERSION["devExtreme"][0:4],
-			'/static/cause-web-template/layouts/MobileLayout.css',
-			'/static/plugins/devExtreme/%s/css/dx.common.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/css/dx.spa.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/layouts/Simple/SimpleLayout.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/layouts/Popup/PopupLayout.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/layouts/SlideOut/SlideOutLayout.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/layouts/Desktop/DesktopLayout.css' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/js/jszip.js' % config.VERSION["devExtreme"],
-			'/static/plugins/devExtreme/%s/js/knockout-%s.js' % (config.VERSION["devExtreme"], config.VERSION["knockout"]),
-			'/static/plugins/devExtreme/%s/js/dx.all.js' % config.VERSION["devExtreme"],
-			'/static/plugins/fontAwesome/%s/css/font-awesome.min.css' % config.VERSION["fontAwesome"],
-			'/static/plugins/jQuery/jquery-%s.js' % config.VERSION["jQuery"],
-			'/static/plugins/cldrjs/%s/cldr.js' % config.VERSION["cldrjs"],
-			'/static/cause-web-javascript/js/addons/date.format.js',
-			'/static/cause-web-javascript/js/cause.js',
-		]
 
 		self.to_head("""
 			<script>
